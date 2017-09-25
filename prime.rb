f2d1092da4db339baf52fb7083d60c1806db1d94
Adam Fudge <coolfudge@gmail.com>
2017-09-25 23:32:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,14 @@
 def prime(int)
+  is_prime = true
+
+  a = 2
+
+  while a < int / 2
+    b = int / a
+    if b == int * a
+      is_prime = false
+      return is_prime
+    end
+  end
+  is_prime
 end
