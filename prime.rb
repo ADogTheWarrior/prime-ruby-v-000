212c125e5fcb0bb96dcb25fee0092b3a2b32bb4a
Adam Fudge <coolfudge@gmail.com>
2017-09-25 23:39:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,8 @@
 def prime(int)
-  is_prime = true
-  a = 2
-  while a < int / 2
-    b = int / a
-    if b == int * a
-      is_prime = false
-      return is_prime
-    end
-    a += 1
+  if int == 2
+    return true
+  elsif int.even?
+    return false
   end
-  is_prime
+
 end
-
-
-puts prime(5)
-puts "5 should be true"
-
-puts prime(8)
-puts "8 should be false"
